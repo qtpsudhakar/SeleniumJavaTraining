f3406b09f403b3bf0b6897274eb52fc09655f019
qtpsudhakar <sudhakar@qtpsudhakar>
2021-04-13 18:05:32 +0530

    selenium

---
+++ src/com/automation/basics/Wd2OhrmLogin.java
@@ -0,0 +1,19 @@
+package com.automation.basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Wd2OhrmLogin {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\Software\\chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/");
+
+        driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+        driver.findElement(By.name("txtPassword")).sendKeys("admin123");
+        driver.findElement(By.id("btnLogin")).click();
+
+        driver.quit();
+    }
+}
